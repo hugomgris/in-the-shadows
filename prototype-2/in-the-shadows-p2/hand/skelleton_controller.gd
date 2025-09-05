8bfb3dc1935097a3522986d6f718d3af492f38d1
Hugo Munoz Gris <hmunoz-g@car13s2.42barcelona.com>
2025-09-05 17:25:41 +0200

    New build

--- prototype-2/in-the-shadows-p2/hand/skelleton_controller.gd
+++ prototype-2/in-the-shadows-p2/hand/skelleton_controller.gd
@@ -10,78 +10,81 @@ extends Skeleton3D
 @onready var skeleton_3d: Skeleton3D = %Skeleton3D
 
 var _mouse_input_direction := Vector2.ZERO
-var is_dragging := false
-var initial_bone_rotation: Quaternion
-var target_rotation: Quaternion
-var accumulated_rotation := Vector3.ZERO
+var _is_dragging := false
+var _has_dragged := false
+var _initial_bone_rotation: Quaternion
+var _target_rotation: Quaternion
+var _accumulated_rotation := Vector3.ZERO
+var _is_rotating := false
 
 func _ready():
 	print("Skeleton controller initialized with ", get_bone_count(), " bones")
-	initial_bone_rotation = get_bone_pose_rotation(bone)
-	target_rotation = initial_bone_rotation
+	_initial_bone_rotation = get_bone_pose_rotation(bone)
+	_target_rotation = _initial_bone_rotation
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("left_click"):
-		is_dragging = true
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	elif event.is_action_released("left_click"):
-		is_dragging = false
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if event is InputEventKey:
+		if event.keycode == KEY_1:
+			bone = find_bone("Bone.009")
+		elif event.keycode == KEY_2:
+			bone = find_bone("Bone.010")
+		elif event.keycode == KEY_3:
+			bone = find_bone("Bone.011")
+		elif event.keycode == KEY_4:
+			bone = find_bone("Bone.012")
+		elif event.keycode == KEY_5:
+			bone = find_bone("Bone.007")
+	
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed and not _is_rotating:
+				_is_dragging = true
+				_has_dragged = false
+			elif not event.pressed:
+				if _is_dragging:
+					_is_dragging = false
 
-func _unhandled_input(event: InputEvent) -> void:
-	if (event is InputEventMouseMotion and is_dragging):
-		_mouse_input_direction = event.screen_relative * mouse_sensitivity
+					if not _has_dragged and not _is_rotating:
+						if _accumulated_rotation.x >= deg_to_rad(90):
+							_accumulated_rotation.x = 0.0
+						else:
+							_accumulated_rotation.x += deg_to_rad(30.0)
+							_accumulated_rotation.x = clamp(_accumulated_rotation.x, deg_to_rad(-10), deg_to_rad(90))
+
+						var delta_rotation = Quaternion.from_euler(_accumulated_rotation)
+						_target_rotation = _initial_bone_rotation * delta_rotation
+						_is_rotating = true
+						
+	elif event is InputEventMouseMotion and _is_dragging:
+		_mouse_input_direction = event.relative * mouse_sensitivity * 0.01
+
 
 func _physics_process(delta: float) -> void:
-	if _mouse_input_direction.length() > 0:
-		# Update accumulated rotation based on mouse input
+	if _is_dragging and _mouse_input_direction.length() > 0:
+		_has_dragged = true
+		
 		var rotation_input = Vector3(
-			_mouse_input_direction.x * rotation_speed * delta,
 			0.0,
-			_mouse_input_direction.y * curl_speed * delta
+			0.0,
+			_mouse_input_direction.y * rotation_speed * delta
 		)
-		
-		accumulated_rotation += rotation_input
-		
-		# Apply clamping
-		accumulated_rotation.x = clamp(accumulated_rotation.x, -deg_to_rad(10), deg_to_rad(90))
-		accumulated_rotation.z = clamp(accumulated_rotation.z, -deg_to_rad(10), deg_to_rad(10))  # ±5 degrees = 10 degree span
-		
-		# Calculate target rotation from initial position
-		var delta_rotation = Quaternion.from_euler(accumulated_rotation)
-		target_rotation = initial_bone_rotation * delta_rotation
-		
-		_mouse_input_direction = Vector2.ZERO
-	
-	# Smoothly lerp current rotation towards target
-	var current_rotation = get_bone_pose_rotation(bone)
-	var smooth_rotation = current_rotation.slerp(target_rotation, lerp_speed * delta)
-	set_bone_pose_rotation(bone, smooth_rotation)
 
-	"""
-	func _physics_process(delta: float) -> void:
-	if _mouse_input_direction.length() > 0:
-		var abs_x = abs(_mouse_input_direction.x)
-		var abs_y = abs(_mouse_input_direction.y)
+		_accumulated_rotation += rotation_input
+		_accumulated_rotation.z = clamp(_accumulated_rotation.z, deg_to_rad(-10), deg_to_rad(10))
 
-		var rotation_input = Vector3.ZERO
-
-		if abs_x > abs_y:
-			rotation_input.z = _mouse_input_direction.x * rotation_speed * delta
-		else:
-			rotation_input.x = _mouse_input_direction.y * curl_speed * delta
-
-		accumulated_rotation += rotation_input
-
-		accumulated_rotation.x = clamp(accumulated_rotation.x, -deg_to_rad(90), deg_to_rad(10))
-		accumulated_rotation.y = clamp(accumulated_rotation.z, -deg_to_rad(5), deg_to_rad(5))
-
-		var delta_rotation = Quaternion.from_euler(accumulated_rotation)
-		target_rotation = initial_bone_rotation * delta_rotation
+		var delta_rotation = Quaternion.from_euler(_accumulated_rotation)
+		_target_rotation = _initial_bone_rotation * delta_rotation
 
 		_mouse_input_direction = Vector2.ZERO
-	
-	var current_rotation = get_bone_pose_rotation(bone);
-	var smooth_rotation = current_rotation.slerp(target_rotation, lerp_speed * delta)
-	set_bone_pose_rotation(bone, smooth_rotation)
-	"""
+		_is_rotating = true
+
+
+	if _is_rotating:
+		var current_rotation = get_bone_pose_rotation(bone)
+		var smooth_rotation = current_rotation.slerp(_target_rotation, lerp_speed * delta)
+		set_bone_pose_rotation(bone, smooth_rotation)
+
+		var angle_diff = current_rotation.angle_to(_target_rotation)
+		if angle_diff < deg_to_rad(1.0):
+			set_bone_pose_rotation(bone, _target_rotation)
+			_is_rotating = false
